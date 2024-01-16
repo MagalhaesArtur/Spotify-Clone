4901dea04df342b93d24f85a701dc1a2c22eacea
João Artur Magalhães <joaoartur4590@gmail.com>
2024-01-16 14:20:31 -0300

    integração com o Supabase concluída

---
+++ types.ts
@@ -0,0 +1,54 @@
+import Stripe from "stripe";
+
+export interface UserDetails {
+  id: string;
+  firstName: string;
+  lastName: string;
+  fullName?: string;
+  avatarUrl?: string;
+  billingAddress?: string;
+  paymentMethod?: Stripe.PaymentMethod[Stripe.PaymentMethod.Type];
+}
+
+export interface Subscription {
+  id: string;
+  userId: string;
+  status?: Stripe.Subscription.Status;
+  metadata?: Stripe.Metadata;
+  priceId?: string;
+  quantity?: number;
+  cancelAtPeriodEnd?: boolean;
+  created: string;
+  currentPeriodStart: string;
+  currentPeriodEnd: string;
+  endedAt?: string;
+  cancelAt?: string;
+  canceledAt?: string;
+  trialStart?: string;
+  trialEnd?: string;
+  prices?: Price;
+}
+
+export interface Price {
+  id: string;
+  productId?: string;
+  active?: boolean;
+  description?: string;
+  unitAmount?: number;
+  currency?: string;
+  type?: Stripe.Price.Type;
+  interval?: Stripe.Price.Recurring.Interval;
+  intervalCount?: number;
+  trialPeriodDays?: number | null;
+  metadata?: Stripe.Metadata;
+  products?: Product;
+}
+
+export interface Product {
+  id: string;
+  active?: boolean;
+  name?: string;
+  description?: string;
+  image?: string;
+  metadata?: Stripe.Metadata;
+}
